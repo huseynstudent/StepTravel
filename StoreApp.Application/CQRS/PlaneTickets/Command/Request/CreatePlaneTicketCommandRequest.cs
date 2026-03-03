fbae749c38a5f8065028e9ffed93f5acf83eb87c
huseynstudent <huseynstudent5628@gmail.com>
2026-03-03 17:22:06 +0800

    plane command

--- StoreApp.Application/CQRS/PlaneTickets/Command/Request/CreatePlaneTicketCommandRequest.cs
+++ StoreApp.Application/CQRS/PlaneTickets/Command/Request/CreatePlaneTicketCommandRequest.cs
@@ -1,12 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MediatR;
+using StoreApp.Application.CQRS.PlaneTickets.Command.Response;
+using StoreApp.Comman.GlobalResponse.Generics.ResponseModel;
 
-namespace StoreApp.Application.CQRS.PlaneTickets.Command.Request
+namespace StoreApp.Application.CQRS.PlaneTickets.Command.Request;
+
+public class CreatePlaneTicketCommandRequest: IRequest<ResponseModel<CreatePlaneTicketCommandResponse>>
 {
-    public class CreatePlaneTicketCommandRequest
-    {
-    }
+    public string Airline { get; set; }
+    public string Gate { get; set; }
+    public string Plane { get; set; }
+    public string Meal { get; set; }
+    public bool HasCheckedIn { get; set; } = false;
+    public double LuggageKg { get; set; }
 }
--- StoreApp.Application/CQRS/PlaneTickets/Command/Request/UpdatePlaneTicketCommandRequest.cs
+++ StoreApp.Application/CQRS/PlaneTickets/Command/Request/UpdatePlaneTicketCommandRequest.cs
@@ -1,12 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MediatR;
+using StoreApp.Application.CQRS.PlaneTickets.Command.Response;
+using StoreApp.Comman.GlobalResponse.Generics.ResponseModel;
 
-namespace StoreApp.Application.CQRS.PlaneTickets.Command.Request
+namespace StoreApp.Application.CQRS.PlaneTickets.Command.Request;
+
+public class UpdatePlaneTicketCommandRequest:IRequest<ResponseModel<UpdatePlaneTicketCommandResponse>>
 {
-    public class UpdatePlaneTicketCommandRequest
-    {
-    }
+    public int Id { get; set; }
+    public string Airline { get; set; }
+    public string Gate { get; set; }
+    public string Plane { get; set; }
+    public string Meal { get; set; }
+    public bool HasCheckedIn { get; set; } = false;
+    public double LuggageKg { get; set; }
 }
