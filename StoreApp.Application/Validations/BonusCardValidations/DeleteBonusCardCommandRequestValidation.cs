1073a2ff02b6947998d16c7375aeacc0134c735e
Onur-bagirov <onurrbagir2008@gmail.com>
2026-03-15 16:32:22 +0400

    Fill BonusCard

--- StoreApp.Application/Validations/BonusCardValidations/DeleteBonusCardCommandRequestValidation.cs
+++ StoreApp.Application/Validations/BonusCardValidations/DeleteBonusCardCommandRequestValidation.cs
@@ -1,12 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿using FluentValidation;
+using StoreApp.Application.CQRS.BonusCards.Command.Request;
 namespace StoreApp.Application.Validations.BonusCardValidations
 {
-    internal class DeleteBonusCardCommandRequestValidation
+    public class DeleteBonusCardCommandRequestValidation : AbstractValidator<DeleteBonusCardCommandRequest>
     {
+        public DeleteBonusCardCommandRequestValidation()
+        {
+            RuleFor(x => x.Id)
+                .GreaterThan(0).WithMessage("The bonus card ID must be greater than zero !")
+                .NotEmpty().WithMessage("The bonus card ID cannot be empty !");
+        }
     }
 }
