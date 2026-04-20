8e3cec950618b4684336f9f70faeabbce25b9d3f
Onur-bagirov <onurrbagir2008@gmail.com>
2026-04-21 03:16:08 +0400

    change

---
+++ StoreApp.DAL/Migrations/20260420061058_mig3_fixed.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StoreApp.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class mig3_fixed : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ProfilePicture",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ArrivalDate",
+                table: "TrainTickets",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ArrivalDate",
+                table: "PlaneTickets",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ProfilePicture",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "ArrivalDate",
+                table: "TrainTickets");
+
+            migrationBuilder.DropColumn(
+                name: "ArrivalDate",
+                table: "PlaneTickets");
+        }
+    }
+}
